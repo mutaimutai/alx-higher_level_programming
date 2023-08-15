f811072c18f385a42c81d59036d079e8e5e9aa8f
mutaimutai <mutaicaleb7539@gmail.com>
2023-08-15 19:20:24 +0000

    last number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,5 +6,5 @@ if last > 5:
 	print("Last digit of {} is {} and is greater than 5".format(number, last))
 elif last == 0:
 	print("Last digit of {} is {} and is 0".format(number, last))
-elif last < 6 and last != 0:
+elif last > 0:
 	print("Last digit of {} is {} and is greater than 5".format(number, last))
