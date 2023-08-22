4e8e2626aba14b218f9d9703c71fbc381eae79b3
mutaimutai <mutaicaleb7539@gmail.com>
2023-08-22 07:34:50 +0000

    printing a matrixx

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return
+
     for row in matrix:
-        for i in range(len(0, row)):
+        for i in range(len(0,row)):
             if i < len(row) - 1:
                 print("{:d} ".format(row[i]), end="")
             else:
