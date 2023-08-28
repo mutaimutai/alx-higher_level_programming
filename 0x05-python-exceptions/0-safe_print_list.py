4b582dc9ba5ff2875d291176e871cbf6d186077d
mutaimutai <mutaicaleb7539@gmail.com>
2023-08-28 14:53:27 +0000

    prints a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,9 +4,8 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print(my_list[i], end='')
-            count =+ 1
-        except:
+            count += 1
+        except IndexError:
             break
     print()
-    return count
-
+    return (count)
