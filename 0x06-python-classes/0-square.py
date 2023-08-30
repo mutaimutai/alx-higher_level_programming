a372bbdca4a9d7b55343309945db4dc107aac71a
mutaimutai <mutaicaleb7539@gmail.com>
2023-08-30 02:29:02 +0000

    creating a class called square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-"""Define a class called square"""
+"""Define a class Square."""
+
 
 class Square:
-    """This class represents a square"""
-   pass
+    """Represent a square."""
+    pass
