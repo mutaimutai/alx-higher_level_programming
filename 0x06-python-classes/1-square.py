46ee34bbf90ec72467b753b86d16cf30fbcba160
mutaimutai <mutaicaleb7539@gmail.com>
2023-08-30 02:31:21 +0000

    creating a square with a size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): This is the size of the square.
+        """
+        self.__size = size
