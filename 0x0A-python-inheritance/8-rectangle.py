a07bbf0fe61f9caf42af3df96b82d60d9127fa4a
mutaimutai <mutaicaleb7539@gmail.com>
2023-09-13 13:09:46 +0000

    initializing width and height

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,27 +1,6 @@
 #!/usr/bin/python3
 """Defining a class named BaseGeometry"""
-
-
-class BaseGeometry:
-    """This is the class that i have implemented"""
-
-    def area(self):
-        """This method is not impemented"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """This method validates if a value is an integer
-        Args:
-            name: This is a string
-            value: Ths is the value to validate
-        Returns:
-            returns true if the value is an integer
-        """
-
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
