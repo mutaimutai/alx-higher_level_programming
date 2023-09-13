1a448f3ed191fe75c1f4dee06ba10378832aead6
mutaimutai <mutaicaleb7539@gmail.com>
2023-09-13 12:33:20 +0000

    validtes an integer

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defining a class named BaseGeometry"""
+
+
+class BaseGeometry:
+    """This is the class that i have implemented"""
+
+    def area(self):
+        """This method is not impemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """This method validates if a value is an integer
+        Args:
+            name: This is a string
+            value: Ths is the value to validate
+        Returns:
+            returns true if the value is an integer
+        """
+
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValuError("{} must be greater than 0".format(name))
+        
