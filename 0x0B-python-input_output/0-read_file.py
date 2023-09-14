85183677e2aa453dd5dcf1c43cd2b14913d987ae
mutaimutai <mutaicaleb7539@gmail.com>
2023-09-14 14:54:50 +0000

    opening a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""writing a function yhat reads a file"""
+
+
 def read_file(filename=""):
     """This is a function that reads content froma file"""
     with open(filename, encoding="UTF-8") as f:
