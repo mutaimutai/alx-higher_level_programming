a04f2418e5f74c698c89842f805fc3b380c8bab1
mutaimutai <mutaicaleb7539@gmail.com>
2023-09-14 17:56:36 +0000

    printing json representation of a string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Returning the JSON representationof an object(string)"""
 
+
 import json
 def to_json_string(my_obj):
     """This function returns the json representation of a string"""
