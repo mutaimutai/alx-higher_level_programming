ba802d2b4417935cabb9f870fd13f7fc08ca7ccb
Parrot Live user <user@localhost.localdomain>
2023-10-10 09:49:23 +0000

    reading from a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,5 +2,5 @@
 def read_file(filename=""):
     """A function that reads a textfile and prints it out"""
     with open(filename, 'r', encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end='')
 
