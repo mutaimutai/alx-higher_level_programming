8f8271ed44e64cb85a2584935e936e1a26246464
Parrot Live user <user@localhost.localdomain>
2023-10-10 09:57:48 +0000

    reading from a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""Defining a function that prints a file"""
+"""This module defines a text file-reading function"""
 
 
 def read_file(filename=""):
-    """A function that reads a textfile and prints it out"""
-    with open(filename, 'r', encoding="utf-8") as file:
-        print(file.read(), end="")
-
+    """Prints the contents of a UTF8 text file"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
