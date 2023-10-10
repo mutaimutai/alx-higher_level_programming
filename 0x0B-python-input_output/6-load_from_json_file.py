2c89f8e8cb1d2c97d58012904226811fbc61fcf7
Parrot Live user <user@localhost.localdomain>
2023-10-10 15:26:42 +0000

    creating a python object

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Loads from a json file"""
+import json
+
+
+def load_from_json_file(filename):
+    """Creates an objectfrom a json file"""
+    with open(filename, 'r', encoding='UTF8') as file:
+        py_object = json.load(file)
+        return py_object
