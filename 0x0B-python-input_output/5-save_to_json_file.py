03aeac89397d8504c8ca98a3b68a13ccf9f206d4
Parrot Live user <user@localhost.localdomain>
2023-10-10 15:14:38 +0000

    writing to a file a json string

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""writes an Object to a text file"""
+"""writes an Object to a text file using json format"""
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """writes json str from py_object""" 
+    """writes json  to a text filestr from py_object using json format""" 
     with open(filename, 'w', encoding="UTF8") as file:
         json.dump(my_obj, file)
