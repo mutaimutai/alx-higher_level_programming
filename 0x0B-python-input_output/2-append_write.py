65e9c0728fcf0754afd1ba99f1b2f652fba02685
Parrot Live user <user@localhost.localdomain>
2023-10-10 10:40:04 +0000

    Appending to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defining a function that appends a text to file"""
+
+
+def append_write(filename="", text=""):
+    """Appends a text to a file"""
+    with open(filename, 'a', encoding='UTF8') as file:
+        file.write(text)
