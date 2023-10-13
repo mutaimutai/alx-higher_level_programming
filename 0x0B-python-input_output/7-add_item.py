64660443e8495026178b85b56f3f5cc03a07e35c
Parrot Live user <user@localhost.localdomain>
2023-10-13 23:21:45 +0000

    adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,7 +7,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
     py_list = load_from_json_file("add_item.json")
-except:
+except FileNotFoundError:
     py_list = []
 
 if __name__ == "__main__":
