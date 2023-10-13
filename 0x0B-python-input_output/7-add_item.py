0ad76615b8f7def33d23bb0353cf8960ecd8f6b7
Parrot Live user <user@localhost.localdomain>
2023-10-13 22:14:36 +0000

    adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,15 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 """Adds all arguments to a python list and save them to a file"""
-import json
-import os
+import sys
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+try:
+    py_list = load_from_json_file("add_item.json")
+except:
+    py_list = []
 
+if __name__ == "__main__":
+    py_list.extend(sys.argv[1:])
+    save_to_json_file(py_list, "add_item.json")
