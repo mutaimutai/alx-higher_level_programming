9765932554900ea72879298d450c81defc32c4ef
Parrot Live user <user@localhost.localdomain>
2023-10-18 15:08:20 +0300

    creating a new instance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -58,6 +58,6 @@ class Base:
     def create(cls, **dictionary):
         """Method that return an instance with all attributes set"""
         if dictionary and dictionary != {}:
-            r1 = cls(2,3)
+            r1 = cls(2, 3)
             r1.update(**dictionary)
             return r1
