d9571e70e459e4fe62c97589cdd5b6b4516b9096
mutaimutai <mutaicaleb7539@gmail.com>
2024-02-16 12:37:49 +0300

    lists all genres of the show Dexter.

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,5 +3,5 @@ SELECT tv_genres.name AS genre,
 COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
 FROM tv_genres 
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY genre 
+GROUP BY genre
 ORDER BY  number_of_shows DESC;
