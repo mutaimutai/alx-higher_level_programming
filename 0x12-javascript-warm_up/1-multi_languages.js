ce4a4d5a566d7555834b2e15e628e20ae1028b65
mutaimutai <mutaicaleb7539@gmail.com>
2024-03-12 22:41:54 +0300

    removing the semicolons

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 console.
-	log('C is fun');
+	log('C is fun')
 console.
-	log('Python is cool');
+	log('Python is cool')
 console.
-	log('JavaScript is amazing');
+	log('JavaScript is amazing')
