1478234afa169105bbea4d8ccc0d2d6672d936c8
mutaimutai <mutaicaleb7539@gmail.com>
2024-03-13 14:50:31 +0300

    printing a string using arguments

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 const { argv } = process;
 const num = parseInt(argv[2]);
 if (num) {
-  for (let i = 0; i <= num; i++) {
+  for (let i = 0; i < num; i++) {
     console.log('C is fun');
   }
 } else {
